f9a94ccc4fe0303f362c59bd37a6c7c8d683f91e
BigWorm0x <101954335+ahmedoop@users.noreply.github.com>
2025-02-17 18:14:17 +0200

    Create one.cpp

---
+++ one.cpp
@@ -0,0 +1,22 @@
+// Program to say (Hello userName)
+#include <iostream>
+using namespace std;
+
+string UserName()
+{
+	string userName;
+	cout << "Please Enter Your userName: ";
+	cin >> userName;
+	return userName;
+}
+
+void print(string un)
+{
+	cout <<"Hello " << un;
+}
+
+
+int main()
+{
+	print(UserName());
+}
